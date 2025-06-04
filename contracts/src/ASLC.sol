121bace7f4e765badc7412e04f1cadf4c26d9d4a
AslamThachapalli <aslam.develop912@gmail.com>
2025-06-04 22:51:00 +0530

    moved the contracts to the contracts folder

--- contracts/src/ASLC.sol
+++ contracts/src/ASLC.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.13;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+// A Custom ERC Token to simulate the bridge.
+// The Token to be deployed in ETH Sepolia and mint myself some coin to get started with the bridge.
 contract ASLC is ERC20, Ownable {
-    constructor() ERC20("Aslam Coin", "ASLC") Ownable(msg.sender) {}
+    constructor() ERC20("ASLCOIN", "ASLC") Ownable(msg.sender) {}
 
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
--- contracts/src/BASLC.sol
+++ contracts/src/BASLC.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.13;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+// The Bridged Token, which is to be deployed in Base Sepolia.
+// After deployment the token owner ship will be transferred to the BridgeBase contract.
+// The Contract will mint and burn the coin as required.
 contract BASLC is ERC20, Ownable {
-    constructor() ERC20("B Aslam Coin", "BASLC") Ownable(msg.sender) {}
+    constructor() ERC20("BASLCOIN", "BASLC") Ownable(msg.sender) {}
 
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
